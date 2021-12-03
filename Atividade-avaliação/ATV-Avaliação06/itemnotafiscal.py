16ad225cbde072202a75a1873ef2ca9a8e698681
PedroHenriqueSantosRodrigues <pedrohenrique10001@gmail.com>
2021-12-03 18:56:18 -0300

    ATV-Avaliação06

---
+++ Atividade-avaliação/ATV-Avaliação06/itemnotafiscal.py
@@ -0,0 +1,45 @@
+"""
+    Módulo itemnotafiscal 
+    Classe ItemNotaFiscal 
+    Atributos :
+        id         - informado
+        sequencial - informado
+        quantidade - informado
+        produto    - informado
+        valor      - calculado.            
+"""
+from produto import Produto
+from DB import db
+class ItemNotaFiscal(db.Model):
+    __tablename__ = 'TB_ITEM_NF'
+    id = db.Column(db.Integer, primary_key=True)
+    sequencial = db.Column(db.Integer, nullable=False)
+    quantidade = db.Column(db.Integer, nullable=False)
+    produtoID = db.Column(db.Integer, db.ForeignKey("TB_PRODUTO.id"), nullable=False)
+    NotaFiscalID = db.Column(db.Integer, db.ForeignKey("TB_NOTA_FISCAL.id"), nullable=False)
+    def __init__(self, id, sequencial, quantidade, produto):
+        self._id=id
+        self._sequencial=sequencial
+        self._quantidade=quantidade
+        self._produto=produto
+        self._descricao=self._produto.getDescricao()
+        self._valorUnitario=self._produto.getValorUnitario()
+        self._valorItem=float(self._quantidade * self._valorUnitario)
+      
+    def str(self):
+        string="\nId={5} Sequencial={4} Quantidade={3} Produto={2} Valor Unitario={1} Valor Item={0}".format(self._valorItem,
+                                                                                                             self._valorUnitario,
+                                                                                                             self._descricao, 
+                                                                                                             self._quantidade, 
+                                                                                                             self._sequencial, 
+                                                                                                             self._id)
+        return string
+    
+        
+if __name__ == '__main__':
+    produto = Produto(1,100,'Arroz', 5.5) 
+    item=ItemNotaFiscal(1, 1, 12, produto)
+    print(item.str())
+        
+    
+
---
+++ Atividade-avaliação/ATV-Avaliação06/notafiscal.py
@@ -0,0 +1,64 @@
+"""
+    Módulo notafiscal -
+    Classe NotaFiscal - 
+        Atributos :
+            id        - informado.
+            codigo    - informado.
+            data      - informado.
+            cliente   - informado.
+            items     - informado
+            valornota - calculado. 
+"""
+import datetime
+from cliente        import Cliente
+from produto        import Produto
+from itemnotafiscal import ItemNotaFiscal
+from DB import db
+
+class NotaFiscal(db.Model):
+    __tablename__ = 'TB_CLIENTE'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    codigo = db.Column(db.Integer, nullable=False)
+    cnpjcpf = db.Column(db.String(80), nullable=False)
+    tipo = db.Column(db.Integer, nullable=False)
+    def __init__(self, Id, codigo, cliente):
+        self._Id = Id
+        self._codigo=codigo
+        self._cliente=cliente 
+        self._data=datetime.datetime.now()   
+        self._itens=[]
+        self._valorNota=0.0        
+        
+    def setCliente(self, cliente):
+        if isinstance(cliente, Cliente):
+            self._cliente=cliente    
+            
+    def adicionarItem(self, item): 
+        if isinstance(item, ItemNotaFiscal):
+            self._itens.append(item)
+        
+    def calcularNotaFiscal(self):
+        valor=0.0
+        for item in self._itens:
+            valor = valor + item._valorItem
+        self.valorNota=valor
+     
+    def imprimirNotaFiscal(self):
+        Linhas = '-' * 111
+        print(Linhas)
+        print('{:101}{}'.format('NOTA FISCAL',datetime.datetime.now().strftime('%d-%m-%Y')))
+        print('Cliente:{:<10}Nome:{}'.format(self._cliente._codigo, self._cliente._nome))
+        print('CPF/CNPJ:{}'.format(self._cliente._cnpjcpf))
+        print(Linhas)
+        print('ITENS')
+        print(Linhas)
+        print('{:5s}     {:55s} {:>5s}         {:>10s}   {:>15s}'.format('Seq','Descrição','QTD','valor unit','preço'))
+        print('{:^5s} {:^55s}      {:^5s}        {:^10s}      {:^15s}'.format('-'*5,'-'*55,'-'*5,'-'*10,'-'*15))
+        for i in range (0,len(self._itens)):
+            print('{:^5}{:^25}                                     {:^5}        {:^10}         {:^15}'.format(self._itens[i]._sequencial,
+                                                             self._itens[i]._produto._descricao,
+                                                             self._itens[i]._quantidade,
+                                                             self._itens[i]._valorUnitario,
+                                                             self._itens[i]._valorItem))
+            print(Linhas)
